616d0ccef0044e0beda54d4aec831f7adca4283b
yuze.zyz <yuze.zyz@alibaba-inc.com>
2025-04-03 00:54:32 +0800

    fix mcp

--- mcp_central/crawl4ai/mcp.py
+++ mcp_central/crawl4ai/mcp.py
@@ -1,7 +1,21 @@
+import html
+
+import trafilatura
 from crawl4ai import *
+from crawl4ai.async_crawler_strategy import AsyncPlaywrightCrawlerStrategy
+from crawl4ai.browser_manager import BrowserManager
 from fastmcp import FastMCP
 
 mcp = FastMCP("crawl4ai")
+
+
+async def __aexit__(self, exc_type, exc_val, exc_tb):
+    await self.close()
+    # Fix: https://github.com/unclecode/crawl4ai/issues/842
+    BrowserManager._playwright_instance = None
+
+
+AsyncPlaywrightCrawlerStrategy.__aexit__ = __aexit__
 
 
 @mcp.tool()
@@ -13,7 +27,8 @@ async def crawl4ai(website: str) -> str:
             result = await crawler.arun(
                 url=website,
             )
-            return result.metadata['description']
+            markdown = html.escape(trafilatura.extract(str(result.markdown)))
+            return {"content": markdown}
     except Exception:
         import traceback
         return f'Error: {traceback.format_exc()}'

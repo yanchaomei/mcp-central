4e336dd004591912b5ef461033123a1717efcc3e
yuze.zyz <yuze.zyz@alibaba-inc.com>
2025-04-15 17:49:26 +0800

    fix deps

--- examples/lite_research/requirements.sh
+++ examples/lite_research/requirements.sh
@@ -2,4 +2,5 @@ pip install -r ../../mcp_central/crawl4ai/requirements.txt
 python -m playwright install --with-deps chromium
 crawl4ai-setup
 crawl4ai-doctor
-pip install gradio mcp openai -U
+pip install gradio
+pip install mcp openai -U
